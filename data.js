a00cd7850b9b67a7fcbc0232e549971b63f994d2
rahmani toufik <rahmanitoufik1200@gmail.com>
2024-10-28 19:38:47 +0100

    new changesss

--- data.js
+++ data.js
@@ -1,55 +1,59 @@
 const EmployeesNames = [
-    "Amine",'Bader' ,'Béatrice' ,'Fodé' ,'Fortuney' ,
-    'Hichem','Laurent','Masika', 'Najm',   
-    'Sonia' ,'Patrice' ,'william',"Adam","Nadège"
+    "Adam", "Amine", "Bader", "Béatrice", "Fodé", "Fortuney", 
+    "Hichem", "Laurent", "Masika", "Najm", "Nadège", 
+    "Patrice", "Sonia", "William"
 ];
 
 const Presence = [
-    "présent(e)", "absent(e)", "en congés", "malade",
-    "a l'école", "drive"
+    "a l'école", "absent(e)", "drive", "en congés", 
+    "malade", "présent(e)"
 ];
 
 const client = [
-    "Bouygues Telecom", "Free mobile", "Orange france", 
-    "LillyBelle", "Ericsson", "Sofrecom", "Orange Sonatel", 
-    "Orange Mali", "Gendarmerie", "AFD Tech", "ARCEP",
-    "MWINGZ","Togocom","TechMahindra", "SFR","QOSI","Proximus",
-    "Talent Group","Telecom Cameroun", "Mozark","Monaco Telecom",
-    "IMT Atlantique","Expresso Telecom","Accenture", "DGA"
+    "AFD Tech", "Accenture", "ARCEP france", "ARCEP togo", 
+    "Bouygues Telecom", "DGA", "Ericsson", "Expresso Telecom", 
+    "Free mobile", "Gendarmerie", "IMT Atlantique", 
+    "LillyBelle", "Monaco Telecom", "Mozark", "MWINGZ", 
+    "Orange france", "Orange Mali", "Orange Sonatel", 
+    "Proximus", "QOSI", "SFR", "Sofrecom", 
+    "Talent Group", "TechMahindra", "Telecom Cameroun", 
+    "Togocom", "autre"
 ];
 
 const activity = [
-    "Test reseau","Test logiciel", "Test Materiel",
-    "Développement(Dev)","Support client", "Drive",
-    "Préparation des slides","Redaction des rapport","Redaction des ModOp", 
-    "Analyse des resultats", "Reunion interne", "Cordination avec fournisseur",
-    "Planification des tests", "Maintenance", "Formation", "Veille technologique",
-    "Audit interne/externe","livraison","inventaire", "suivi driver" 
+    "Analyse des resultats", "Audit interne/externe", "Cordination avec fournisseur", 
+    "Développement(Dev)", "Drive", "Formation", 
+    "inventaire", "livraison", "Maintenance", 
+    "Planification des tests", "Préparation des slides", 
+    "Réunion interne", "Rédaction des ModOps", "Rédaction des rapports", 
+    "Suivi driver", "Support client", "Test logiciel", 
+    "Test Matériel", "Test réseau", "Veille technologique"
 ];
 
 const ProblèmeRencontré = [
-    "Pas de problème","Manque de ressource","Manque d'information",
-    "retard de livraison", "repture de stock", "problème de materiel/logiciel",
-    "problème de configuration réseau","Delai de reponse  fournisseur",
-    "incompatibilite logicielle", 'problème de sécurité', "interruption de service",
-    "problème de communication interne", "Erreurs de donnees",
-    "problèmes d'intégration  systeme", "problème de cordination avec l'équipe technique"
+    "Délai de réponse fournisseur", "Erreurs de donnees", 
+    "Incompatibilite logicielle", "Interruption de service", 
+    "Manque d'information", "Manque de ressource", "Pas de problème", 
+    "Problème de communication interne", "Problème de configuration réseau", 
+    "Problème de cordination avec l'équipe technique", 
+    "Problème de materiel/logiciel", "Problème de sécurité", 
+    "Problèmes d'intégration systeme", "Repture de stock", 
+    "Retard de livraison"
 ];
 
 const statut = [
-    "fait", "En cours", "Non démarré", "En attente de validation ", "Reporté", "Annulé"
+    "Annulé", "En attente de validation", "En cours", 
+    "Fait", "Non démarré", "Reporté"
 ];
 
-
-const  Fournisseur = [
-    "accuver","ericsson","amdocs", "narda", "pctel", "alpha wave","pas de fournisseur"
+const Fournisseur = [
+    "Accuver", "Alpha wave", "Amdocs", "Ericsson", "Narda", "Pctel", "Pas de fournisseur"
 ];
 
 const today = new Date();
 const formattedDate = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
 
-const Opportunité = ["pas d'opportunités","Opportunité"]
-
+const Opportunité = ["Opportunité", "pas d'opportunités"];
 
 const data = {
     EmployeesNames,
